f8e023259af24fda1f4cbf678f776d1950e36f4d
Jeremy Ward <jeremy@jmichaelward.com>
2021-11-06 10:32:34 -0500

    Add minimum requirements for making a collection request.

--- src/User/Collection.php
+++ src/User/Collection.php
@@ -9,8 +9,37 @@ use JMichaelWard\BoardGameGeek\OptionsInterface;
  */
 class Collection implements OptionsInterface
 {
+    /**
+     * The username identifying which collection to retrieve.
+     *
+     * @var string
+     */
+    private string $username;
+
+    /**
+     * The subtype of the collection to retrieve.
+     *
+     * May be one of: boardgame, boardgameexpansion, boardgameaccessory, rpgitem, rpgissuse, or videogame.
+     *
+     * @var string
+     */
+    private string $subtype = 'boardgame';
+
+    /**
+     * Class constructor.
+     *
+     * @param string $username Username identifying which collection to retrieve.
+     */
+    public function __construct( string $username )
+    {
+        $this->username = $username;
+    }
+
     public function get_options(): array
     {
-        return [];
+        return [
+            'username' => $this->username,
+            'subtype'  => $this->subtype,
+        ];
     }
 }
