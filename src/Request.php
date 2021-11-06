bc90187fb4d62d9dac00256aaead0b4582da4a39
Jeremy Ward <jeremy@jmichaelward.com>
2021-11-06 10:23:19 -0500

    Move xml and json selection to helper methods.

--- src/Request.php
+++ src/Request.php
@@ -7,11 +7,38 @@ use JMichaelWard\BoardGameGeek\User\User;
 class Request {
 	const BASE_URL = 'https://www.boardgamegeek.com/xmlapi2';
 
+    /**
+     * The response format to return.
+     *
+     * The BoardGameGeek API uses XML by default, but this library defaults to JSON. Users of this library can opt
+     * instead for XML output by calling `set_xml_format` on this object.
+     *
+     * @var string
+     */
+    private string $format = 'json';
+
+    /**
+     * Sets the response format to XML.
+     */
+    public function set_xml_format(): void {
+        $this->format = 'xml';
+    }
+
+    /**
+     * Converts the response to JSON unless XML output is desired.
+     *
+     * @param string $response The XML response from the API request.
+     * @return string
+     */
+    private function get_parsed_response( string $response ): string {
+        return $this->format === 'xml' ? $response : json_encode(simplexml_load_string($response));
+    }
+
 	public function getHot(): string {
 		return '';
 	}
 
-	public function getCollection( OptionsInterface $collection, $format = 'json' ): string {
+	public function getCollection( OptionsInterface $collection ): string {
         $query = http_build_query( $user->get_options() );
         $curl  = curl_init( self::BASE_URL . "/collection?{$query}" );
 
@@ -23,7 +50,7 @@ class Request {
 
         curl_close($curl);
 
-        return $format === 'xml' ? $response : json_encode(simplexml_load_string($response));
+        return $this->get_parsed_response($response);
 	}
 
 	public function getPlays(): string {
@@ -44,7 +71,7 @@ class Request {
      * @param OptionsInterface $user
      * @return string
      */
-	public function getUser( OptionsInterface $user, $format = 'json' ): string {
+	public function getUser( OptionsInterface $user ): string {
 		$query = http_build_query( $user->get_options() );
 		$curl  = curl_init( self::BASE_URL . "/user?{$query}" );
 
@@ -56,6 +83,6 @@ class Request {
 
 		curl_close($curl);
 
-		return $format === 'xml' ? $response : json_encode(simplexml_load_string($response));
+		return $this->get_parsed_response($response);
 	}
 }
